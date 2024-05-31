9f370dfe063827a6d0ba9cff91fdc9e22cb71621
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-05-31 16:32:13 +0200

    modificato effetto scroll

--- U2-D5/base_di_partenza/assets/js/script.js
+++ U2-D5/base_di_partenza/assets/js/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("scroll", function () {
   const button = document.getElementById("scroll-btn");
-  const nav = document.getElementsByName("nav");
-  if (window.scrollY > 50) {
+  const nav = document.getElementById("navBar");
+  if (window.scrollY > 240) {
     button.classList.add("scrolled");
     nav.classList.add("scrolled");
   } else {
