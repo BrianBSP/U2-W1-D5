84ab7f138903432fffc3f460073f76ae2aeaa1f5
BrianBSP <167891999+BrianBSP@users.noreply.github.com>
2024-06-03 23:45:50 +0200

    aggiunta animazione delle M

--- U2-D5/base_di_partenza/assets/js/script.js
+++ U2-D5/base_di_partenza/assets/js/script.js
@@ -1,3 +1,4 @@
+/* transition della navbar allo scroll della pagina */
 document.addEventListener("scroll", function () {
   const button = document.getElementById("scroll-btn");
   const nav = document.getElementById("navBar");
@@ -9,3 +10,19 @@ document.addEventListener("scroll", function () {
     nav.classList.remove("scrolled");
   }
 });
+/* animantion delle M */
+
+const m = document.querySelectorAll("g");
+const mArray = [...m];
+
+const mZeroArray = mArray.filter((element) => element.getAttribute("opacity") === "0");
+console.log(mZeroArray);
+
+const accendiEmme = setInterval(() => {
+  let random = Math.round(Math.random() * mZeroArray.length);
+  if (mZeroArray[random].getAttribute("opacity") === "0") {
+    mZeroArray[random].setAttribute("opacity", 1);
+  } else {
+    mZeroArray[random].setAttribute("opacity", 0);
+  }
+}, 40);
